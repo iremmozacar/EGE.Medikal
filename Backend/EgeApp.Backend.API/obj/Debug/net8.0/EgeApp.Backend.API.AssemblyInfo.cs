3a01e605b63072753cde348889f806b3e6fcb702
iremozacar <iremozacaar@gmail.com>
2024-11-30 19:58:56 +0300

    .

--- Backend/EgeApp.Backend.API/obj/Debug/net8.0/EgeApp.Backend.API.AssemblyInfo.cs
+++ Backend/EgeApp.Backend.API/obj/Debug/net8.0/EgeApp.Backend.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EgeApp.Backend.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a1512d53cd746b5b27d47533b7e6169ebadb3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b00bb20e922b20ab3ef62568dd72ef75d69132cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("EgeApp.Backend.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EgeApp.Backend.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
